eacf49878d446f898061b25a1600966a46dd984a
AndDev <anddesenvolved@gmail.com>
2024-07-03 13:54:33 -0300

    First commit - configuration

---
+++ src/main/java/com/beyou/dreamsBoard/DreamsBoardApplication.java
@@ -0,0 +1,13 @@
+package com.beyou.dreamsBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamsBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamsBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beyou/dreamsBoard/DreamsBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyou.dreamsBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamsBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
