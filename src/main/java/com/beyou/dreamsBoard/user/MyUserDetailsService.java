e89601c09f827729802fe09d0cafba5c8f408954
AndDev <anddesenvolved@gmail.com>
2024-07-04 13:54:50 -0300

    Register user mthod created and changes is UserDetailsService

--- src/main/java/com/beyou/dreamsBoard/user/MyUserDetailsService.java
+++ src/main/java/com/beyou/dreamsBoard/user/MyUserDetailsService.java
@@ -3,22 +3,24 @@ package com.beyou.dreamsBoard.user;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class MyUserDetailsService implements UserDetailsService {
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-    public MyUserDetailsService(UserRepository repository){
+    public MyUserDetailsService(UserRepository repository, PasswordEncoder passwordEncoder){
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User registerNewUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return repository.save(user);
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = repository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        return new MyUserDetails(user);
-    }
-
-    public UserDetails loadUserByEmail(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = repository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Email not found"));
         return new MyUserDetails(user);
